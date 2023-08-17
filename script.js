ce3709e8f27f30c31110a4abfd96cf82f1ce90e2
Ely Carvalho Da Silva <f019435@aethra.com.br>
2023-08-17 02:47:52 -0300

    .

--- script.js
+++ script.js
@@ -40,5 +40,27 @@ class Dados{
 		localStorage.setItem(codigo, JSON.stringify(d))
 		localStorage.setItem('codigo', codigo)
 	}
+
+	recuperaRegistros(){
+		let dadosClientes = Array()
+		let codigo = localStorage.getItem('codigo')
+
+		//recupera registros de todos os clientes cadastrados
+		for(let i = 1; i <= codigo; i++){
+            codigoCliente = JSON.parse(localStorage.getItem(i))
+
+            //pular indices removidos
+            if(codigoCliente === null){
+            	continue
+            }
+            codigoCliente.codigo = i
+            dadosClientes.push(codigoCliente)
+		}
+		return dadosClientes
+	}
+
+	pesquisar(dados){
+		let dadosFiltrados = Array()
+	}
 }
 
